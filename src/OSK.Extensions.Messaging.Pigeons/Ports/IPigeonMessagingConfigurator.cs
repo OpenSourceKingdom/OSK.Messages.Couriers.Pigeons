d29b796765d3868174017ce92114fee849749664
BlankDev117 <mmmasent@gmail.com>
2026-07-23 08:32:54 -0400

    clean

--- src/OSK.Extensions.Messaging.Pigeons/Ports/IPigeonMessagingConfigurator.cs
+++ src/OSK.Extensions.Messaging.Pigeons/Ports/IPigeonMessagingConfigurator.cs
@@ -2,10 +2,12 @@
 using OSK.Messages.Couriers.Pigeons.Options;
 using OSK.Messages.Messaging.Ports;
 using System;
-using OSK.Extensions.Messaging.Pigeons.Ports;
 
 namespace OSK.Extensions.Messaging.Pigeons.Ports;
 
+/// <summary>
+/// A configurator that is able to configure both the pigeon courier and messaging system
+/// </summary>
 [HexagonalIntegration(HexagonalIntegrationType.LibraryProvided)]
 public interface IPigeonMessagingConfigurator
 {
